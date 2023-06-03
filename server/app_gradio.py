f03c0a3d4843b033f234211d9a61d05b30beaff9
Minh Hiếu <75494633+mhieu1002@users.noreply.github.com>
2023-06-03 12:08:25 +0700

    upload server

--- server/app_gradio.py
+++ server/app_gradio.py
@@ -13,6 +13,10 @@ from denoiser import pretrained
 from denoiser.dsp import convert_audio
 from IPython import display as disp
 import re
+import pydub
+from pydub.silence import split_on_silence
+from scipy.signal import butter, filtfilt
+from underthesea import sent_tokenize
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -47,32 +51,52 @@ load_models(parser, arguments, args, unk_args)
 remove_noise_model = pretrained.dns64()
 
 MAX_LENGTH = 30
+
 def text_to_speech(text_input):
     text_cleaned = tp.clean_text(text_input)
     if text_cleaned[-1] not in '?.!':
         text_cleaned += '.'
+    # sentences = sent_tokenize(text_cleaned)
     text_cleaned = re.sub("(?<![.!?])[\n]+", ".\n", text_cleaned)
     sentences, breaks = split_sentences(text_cleaned, MAX_LENGTH)
-    result = infer_mb_melgan([text_cleaned], args)
-    audios, rate, perf = result
-    audios = list(audios)
-    # for i, _break in enumerate(breaks):
-        # audios.insert(_break+i, list(AudioSegment.silent(500, 22050).raw_data))
+
+    audios = []
+    for sentence in sentences:
+        result = infer_mb_melgan([sentence], args)
+        audio, rate, perf = result
+        audio = audio[0]
+        audios.append(audio)
         
-    audio = np.concatenate(audios)
-    #audio /= (1.414 / 32767)
-    audio = audio.astype(np.int16)
-    input_path='output/test.wav'
-    write_wav.write(input_path, rate, audio)
+    audio_combined = np.concatenate(audios)
+    audio_combined = audio_combined.astype(np.int16)
+    input_path = 'output/test.wav'
+    write_wav.write(input_path, rate, audio_combined)
     
-    wav, sr = torchaudio.load(input_path)
-    wav_save = convert_audio(wav, sr, 22050, remove_noise_model.chin)
-    with torch.no_grad():
-        denoised = remove_noise_model(wav_save[None])[0]
-    audio_final = disp.Audio(denoised.data.cpu().numpy(), rate=22050)
-    save_tmp='output/test1.wav'
-    with open(save_tmp, 'wb') as fa:
-        fa.write(audio_final.data)
+    # Load and process the audio file
+    audio = AudioSegment.from_file(input_path)
+    audio = audio.set_channels(1)  # Convert to mono
+    audio = audio.set_frame_rate(22050)  # Set sample rate to 22050 Hz
+    
+    # Split audio on silence
+    chunks = split_on_silence(audio, min_silence_len=500, silence_thresh=-36, keep_silence=500)
+    
+    # Process and filter chunks
+    processed_chunks = []
+    for chunk in chunks:
+        # Apply additional noise reduction or filtering if needed
+        processed_chunk = chunk
+        processed_chunk = processed_chunk.low_pass_filter(1000)  # Apply a low-pass filter
+        processed_chunk = processed_chunk.normalize()  # Normalize the audio volume
+        processed_chunks.append(processed_chunk)
+    
+    # Concatenate processed chunks
+    combined_audio = processed_chunks[0]
+    for segment in processed_chunks[1:]:
+        combined_audio += segment
+    
+    # Export the final audio
+    save_tmp = 'output/test1.wav'
+    combined_audio.export(save_tmp, format="wav")
     
     return save_tmp
 
